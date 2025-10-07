5849849fa2e23722ef9a8ac718266d8265cd2084
Raiyana Rahman <raiyana2@ualberta.ca>
2025-10-06 23:18:58 -0600

    code and source buttons work

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -30,6 +30,10 @@ const Projects = () => (
               ))}
             </TagList>  
           </div>
+          <UtilityList>
+            <ExternalLinks href={visit}>Code</ExternalLinks>
+            <ExternalLinks href={source}>Source</ExternalLinks>
+          </UtilityList>
         </BlogCard>
     ))}
     </GridContainer>
